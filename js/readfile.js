e27be4a142fd0cfa515ce110a502bedb04524a2f
NemoChen <nemochentw@gmail.com>
2016-05-24 17:23:19 +0800

    新增 存檔功能.

--- js/readfile.js
+++ js/readfile.js
@@ -33,6 +33,39 @@
       reader.readAsText(file);
   }
 
+  function saveFile()
+  {
+  	var textToWrite = document.getElementById("parse_content").textContent;
+  	var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+    var fileNameToSaveAs = "NewFile"
+  	// var fileNameToSaveAs = document.getElementById("inputFileNameToSaveAs").value;
+
+  	var downloadLink = document.createElement("a");
+  	downloadLink.download = fileNameToSaveAs;
+  	downloadLink.innerHTML = "Download File";
+  	if (window.webkitURL != null)
+  	{
+  		// Chrome allows the link to be clicked
+  		// without actually adding it to the DOM.
+  		downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+  	}
+  	else
+  	{
+  		// Firefox requires the link to be added to the DOM
+  		// before it can be clicked.
+  		downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+  		downloadLink.onclick = destroyClickedElement;
+  		downloadLink.style.display = "none";
+  		document.body.appendChild(downloadLink);
+  	}
+
+  	downloadLink.click();
+  }
+  function destroyClickedElement(event)
+  {
+  	document.body.removeChild(event.target);
+  }
+
 
   function parseFile(FileContens) {
 
